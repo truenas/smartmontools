e01eea4955fe646a5880e0cedc863bb513f08155
Ameer Hamza <ahamza@ixsystems.com>
2024-11-05 02:26:08 +0500

    Switch to a local repository
    Switch to a local repository for smartmontools to incorporate the fixes
    for NAS-131327 and NAS-131181.

--- pull.sh
+++ pull.sh
@@ -9,3 +9,11 @@ rm smartmontools_$VERSION-$REVISION.debian.tar.xz
 wget http://deb.debian.org/debian/pool/main/s/smartmontools/smartmontools_$VERSION.orig.tar.xz
 tar xf smartmontools_$VERSION.orig.tar.xz --strip 1
 rm smartmontools_$VERSION.orig.tar.xz
+
+cp NAS-131181.patch debian/patches
+cp NAS-131327.patch debian/patches
+echo 'NAS-131181.patch' >> debian/patches/series
+echo 'NAS-131327.patch' >> debian/patches/series
+
+cat changelog debian/changelog > temp_changelog
+mv -f temp_changelog debian/changelog
